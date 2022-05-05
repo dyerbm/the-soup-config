0b851c0c7a1959feb4f58dc410fa5d275639ab2b
dyerbm <dyerbm@mcmaster.ca>
2022-05-05 01:05:25 -0400

    Improved how org-ref is called, added hugo backend

--- config.el
+++ config.el
@@ -117,7 +117,21 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (setq org-log-done t)
 
-(require 'org-ref)
+(use-package org-ref
+  :ensure t
+  :init
+  (with-eval-after-load 'ox
+    (defun my/org-ref-process-buffer--html (backend)
+      "Preprocess `org-ref' citations to HTML format.
+
+Do this only if the export backend is `html' or a derivative of
+that."
+      ;; `ox-hugo' is derived indirectly from `ox-html'.
+      ;; ox-hugo <- ox-blackfriday <- ox-md <- ox-html
+      (when (org-export-derived-backend-p backend 'html)
+        (org-ref-process-buffer 'html)))
+    (add-to-list 'org-export-before-parsing-hook #'my/org-ref-process-buffer--html)))
+
 (setq reftex-default-bibliography '("~/Documents/bibliography/references.bib"))
 
 (setq org-ref-bibliography-notes "~/Documents/bibliography/notes.org"
